82c9a9d22e6a0de938798b90efc3d3f83ea62e12
NIRAVENDLOS <nirav@endlos.in>
2022-05-27 18:53:36 +0530

    url change

--- src/app/@service/service.ts
+++ src/app/@service/service.ts
@@ -1,12 +1,9 @@
 
 
 export const apiUrl = {
-<<<<<<< HEAD
-//  url : 'http://localhost:9090'
-   url : 'http://www.unitechspin.com:8080/unitechSpin'
-=======
+
   // url : 'http://localhost:9090'
   url : 'http://www.unitechspin.com:8080/unitechSpin'
->>>>>>> devlopment
+
 }
   
